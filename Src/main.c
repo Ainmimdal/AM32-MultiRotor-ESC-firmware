a039076c2df92f89a4e742b40cebc45447b58db6
Ainmimdal <67187217+Ainmimdal@users.noreply.github.com>
2024-10-10 14:56:07 +0800

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -217,7 +217,7 @@
 //=============================  Defaults =============================
 //===========================================================================
 
-uint8_t drive_by_rpm = 0;
+uint8_t drive_by_rpm = 1;
 uint32_t MAXIMUM_RPM_SPEED_CONTROL = 5000;
 uint32_t MINIMUM_RPM_SPEED_CONTROL = 500;
 
@@ -263,19 +263,19 @@ char eeprom_layout_version = 2;
 char dir_reversed = 0;
 char comp_pwm = 1;
 char VARIABLE_PWM = 1;
-char bi_direction = 0;
+char bi_direction = 1;
 char stuck_rotor_protection = 1;	// Turn off for Crawlers
-char brake_on_stop = 0;
-char stall_protection = 0;
-char use_sin_start = 0;
+char brake_on_stop = 1;
+char stall_protection = 1;
+char use_sin_start = 1;
 char TLM_ON_INTERVAL = 0;
 uint8_t telemetry_interval_ms = 30;
 uint8_t TEMPERATURE_LIMIT = 255;  // degrees 255 to disable
 char advance_level = 2;			// 7.5 degree increments 0 , 7.5, 15, 22.5)
-uint16_t motor_kv = 2000;
+uint16_t motor_kv = 1850;
 char motor_poles = 14;
 uint16_t CURRENT_LIMIT = 202;
-uint8_t sine_mode_power = 5;
+uint8_t sine_mode_power = 8;
 char drag_brake_strength = 10;		// Drag Brake Power when brake on stop is enabled
 uint8_t driving_brake_strength = 10;
 uint8_t dead_time_override = DEAD_TIME;
